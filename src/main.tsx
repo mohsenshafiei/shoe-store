e46d1ead7c4276dfdddce0a904a7829f7ffe4b22
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2024-08-13 12:59:16 -0400

    feat(app): update test file and add router and Home component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,36 @@
-import './styles/index.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
 
-export function sum(a: number, b: number): number {
-  return a + b;
-}
+// Pages
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+
+// Styles
+import './style.scss';
+
+// Layout
+import Layout from './layouts';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
