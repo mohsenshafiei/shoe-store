0ecc65eccd24d5fd9961d86fcc9310e797085c90
mohsenshafiei <mohsenshafieitafreshi@gmail.com>
2024-08-13 13:08:34 -0400

    feat(app): add shoes and stores and legends constant values

---
+++ src/utils/constant.ts
@@ -0,0 +1,47 @@
+export const LEGENDS_DATA = [
+  { id: '1', color: '#4F46E5', label: 'High Inventory' },
+  { id: '2', color: '#6366F1', label: 'Moderate Inventory' },
+  { id: '3', color: '#818CF8', label: 'Low Inventory' },
+  { id: '4', color: '#A5B4FC', label: 'Restocked Recently' },
+  { id: '5', color: '#C7D2FE', label: 'Out of Stock' },
+  { id: '6', color: '#09090B', label: 'NA' },
+];
+
+export const STORE_LIST = [
+  'ALDO Centre Eaton',
+  'ALDO Destiny USA Mall',
+  'ALDO Pheasant Lane Mall',
+  'ALDO Holyoke Mall',
+  'ALDO Maine Mall',
+  'ALDO Crossgates Mall',
+  'ALDO Burlington Mall',
+  'ALDO Solomon Pond Mall',
+  'ALDO Auburn Mall',
+  'ALDO Waterloo Premium Outlets',
+];
+
+export const SHOE_LIST = [
+  'ADERI',
+  'MIRIRA',
+  'CAELAN',
+  'BUTAUD',
+  'SCHOOLER',
+  'SODANO',
+  'MCTYRE',
+  'CADAUDIA',
+  'RASIEN',
+  'WUMA',
+  'GRELIDIEN',
+  'CADEVEN',
+  'SEVIDE',
+  'ELOILLAN',
+  'BEODA',
+  'VENDOGNUS',
+  'ABOEN',
+  'ALALIWEN',
+  'GREG',
+  'BOZZA',
+];
+
+export const LOW_STOCK_THRESHOLD = 10;
+export const HIGH_STOCK_THRESHOLD = 80;
